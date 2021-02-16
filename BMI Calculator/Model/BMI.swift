56a106ba160282743d627b951c6d1e5706ccdaa6
Taras Bilokhvost <taras_bilohvost@icloud.com>
2021-02-16 16:55:02 +0200

    Application works.

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Macbook on 16.02.2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
